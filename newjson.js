9a2b3e6196dce0aaae53d69af9ec300bbba612a6
HaiderInvo <97734081+HaiderInvo@users.noreply.github.com>
2022-03-11 14:58:03 +0500

    Create newjson.js

---
+++ newjson.js
@@ -0,0 +1,28 @@
+{
+  "collection": {
+    "name": "hello collection",
+    "description": "hello description",
+    "id": "123"
+  },
+  "name": "mynft",
+  "symbol": "cyu",
+  "description": "nothing here",
+  "seller_fee_basis_points": 1000,
+  "image": "https://gateway.pinata.cloud/ipfs/QmRqJarRKxVr65Rp3nZKygQd39gavbjPjGqoWmetX8ZVLw",
+  "artistName": "nobody",
+  "properties": {
+    "hello_world": {
+      "i am nobody": "yes you are right"
+    },
+    "files": {
+      "uri": "https://gateway.pinata.cloud/ipfs/QmRqJarRKxVr65Rp3nZKygQd39gavbjPjGqoWmetX8ZVLw",
+      "type": "image/png"
+    },
+    "creators": [
+      {
+        "address": "BegUZruVk9j3bQ3fU29uzLsNeqkpEdqsz2AXbrLcweem",
+        "share": 100
+      }
+    ]
+  }
+}
